cef536ed8074f1fdd39eee34dc690652d0c787c3
LiYang Dong <119646676+yituanxing@users.noreply.github.com>
2026-08-28 20:03:03 +0800

    linux: define NOMMU MiniMachine page model

---
+++ linux-overlay/arch/minimachine/include/asm/page.h
@@ -0,0 +1,7 @@
+#ifndef _ASM_MINIMACHINE_PAGE_H
+#define _ASM_MINIMACHINE_PAGE_H
+
+/* First MiniMachine Linux target is 4 KiB-page NOMMU with identity mapping. */
+#include <asm-generic/page.h>
+
+#endif /* _ASM_MINIMACHINE_PAGE_H */
