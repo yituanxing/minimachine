ffb1c874684b26699b64b4a41002b26e3c6af8de
LiYang Dong <119646676+yituanxing@users.noreply.github.com>
2026-08-28 22:32:48 +0800

    linux: include task stack contract in boot runtime

--- linux-overlay/arch/minimachine/kernel/bootstrap.c
+++ linux-overlay/arch/minimachine/kernel/bootstrap.c
@@ -18,6 +18,7 @@
 #include <linux/sched.h>
 #include <linux/sched/debug.h>
 #include <linux/sched/task.h>
+#include <linux/sched/task_stack.h>
 #include <linux/seq_file.h>
 #include <linux/string.h>
 
