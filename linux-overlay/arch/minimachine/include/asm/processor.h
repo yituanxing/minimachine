ba1445a8dea54cefae67da31c035c0f3044f74e6
LiYang Dong <119646676+yituanxing@users.noreply.github.com>
2026-08-28 21:53:00 +0800

    linux: define MiniMachine user start-thread transition

--- linux-overlay/arch/minimachine/include/asm/processor.h
+++ linux-overlay/arch/minimachine/include/asm/processor.h
@@ -32,6 +32,22 @@ struct thread_struct {
 #define KSTK_EIP(task)	(task_pt_regs(task)->pc)
 #define KSTK_ESP(task)	(task_pt_regs(task)->sp)
 
+static inline void start_thread(struct pt_regs *regs,
+				unsigned long pc,
+				unsigned long sp)
+{
+	unsigned int i;
+
+	regs->pc = pc;
+	regs->sp = sp;
+	for (i = 0; i < 6; ++i)
+		regs->args[i] = 0;
+	regs->result = 0;
+	regs->syscall_nr = ~0UL;
+	regs->orig_arg0 = 0;
+	regs->status = MINIMACHINE_STATUS_USER | MINIMACHINE_STATUS_IRQ_ENABLE;
+}
+
 #define cpu_relax() barrier()
 
 extern unsigned long __get_wchan(struct task_struct *task);
