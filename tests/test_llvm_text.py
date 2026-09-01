4d4193a7a9449f687e55449bfcebdbd686094350
LiYang Dong <119646676+yituanxing@users.noreply.github.com>
2026-09-02 00:43:39 +0800

    test: freeze LLVM switch metadata boundary

--- tests/test_llvm_text.py
+++ tests/test_llvm_text.py
@@ -26,3 +26,31 @@ define void @g(%struct.foo* %value, void (i32)* %callback, i64 %count) {
     functions = parse_module(module)
     assert len(functions) == 1
     assert functions[0].args == ("%value", "%callback", "%count")
+
+
+def test_switch_metadata_does_not_consume_following_blocks():
+    module = r'''
+define i64 @f(i8 %x) {
+entry:
+  switch i8 %x, label %other [
+    i8 3, label %three
+    i8 19, label %nineteen
+  ], !prof !1
+three:
+  ret i64 3
+nineteen:
+  ret i64 19
+other:
+  ret i64 0
+}
+!1 = !{!"branch_weights", i32 1, i32 2, i32 3}
+'''
+    functions = parse_module(module)
+    assert len(functions) == 1
+    fn = functions[0]
+    assert [block.label for block in fn.blocks] == [
+        "entry", "three", "nineteen", "other"
+    ]
+    assert len(fn.blocks[0].instructions) == 1
+    assert fn.blocks[0].instructions[0].opcode == "switch"
+    assert fn.blocks[0].instructions[0].text.endswith("], !prof !1")
