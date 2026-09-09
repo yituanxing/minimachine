9553658dbd3c1445ce818c6a6e3f838b3e084e1f
LiYang Dong <119646676+yituanxing@users.noreply.github.com>
2026-09-09 17:20:25 +0800

    perf: pack P3 instructions into four operands

--- src/minimachine/native_vm.py
+++ src/minimachine/native_vm.py
@@ -83,7 +83,7 @@ MM_STATUS_WATCH = 3
 MM_STATUS_ERROR = 4
 _NATIVE_PAGE_SIZE = 65536
 _NATIVE_PACK_MAGIC = b"MMP3NP1\0"
-_NATIVE_PACK_VERSION = 1
+_NATIVE_PACK_VERSION = 2
 _NATIVE_PACK_HEADER = struct.Struct("<8sIIIIQQQQ32s40x")
 _NATIVE_PACK_HEADER_SIZE = _NATIVE_PACK_HEADER.size
 
@@ -202,12 +202,10 @@ class CInst(ctypes.Structure):
         ("extend", ctypes.c_uint8),
         ("src_bits", ctypes.c_uint8),
         ("_pad", ctypes.c_uint8 * 3),
-        ("dst", COperand),
-        ("src", COperand),
-        ("a", COperand),
-        ("b", COperand),
-        ("t", COperand),
-        ("f", COperand),
+        ("op0", COperand),
+        ("op1", COperand),
+        ("op2", COperand),
+        ("op3", COperand),
     ]
 
 
@@ -1393,14 +1391,14 @@ class NativeVM(VM):
                         "trunc": MM_EXT_TRUNC,
                     }[inst.extend]
                     out.src_bits = inst.src_bits or 0
-                    out.dst = self._operand(inst.dst, linked, program)
-                    out.src = self._operand(inst.src, linked, program)
+                    out.op0 = self._operand(inst.dst, linked, program)
+                    out.op1 = self._operand(inst.src, linked, program)
                 elif isinstance(inst, p3.Sub):
                     out.opcode = MM_OP_SUB
                     out.width = inst.width.value
-                    out.dst = self._operand(inst.dst, linked, program)
-                    out.a = self._operand(inst.a, linked, program)
-                    out.b = self._operand(inst.b, linked, program)
+                    out.op0 = self._operand(inst.dst, linked, program)
+                    out.op1 = self._operand(inst.a, linked, program)
+                    out.op2 = self._operand(inst.b, linked, program)
                 elif isinstance(inst, p3.Br):
                     out.opcode = MM_OP_BR
                     out.width = inst.width.value
@@ -1409,9 +1407,9 @@ class NativeVM(VM):
                         muir.Cond.ULT: MM_COND_ULT,
                         muir.Cond.SLT: MM_COND_SLT,
                     }[inst.cond]
-                    out.a = self._operand(inst.a, linked, program)
-                    out.b = self._operand(inst.b, linked, program)
-                    out.t = self._target(
+                    out.op0 = self._operand(inst.a, linked, program)
+                    out.op1 = self._operand(inst.b, linked, program)
+                    out.op2 = self._target(
                         inst.true_target,
                         function_name,
                         linked,
@@ -1419,7 +1417,7 @@ class NativeVM(VM):
                         host_by_symbol,
                         local_block_index,
                     )
-                    out.f = self._target(
+                    out.op3 = self._target(
                         inst.false_target,
                         function_name,
                         linked,
